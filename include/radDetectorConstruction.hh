37640154bcfecda67fb201658a4a52ae8c9c02e3
Ciprian Gal <ciprian@jlab.org>
2017-04-28 13:46:41 -0400

    update to new configuration

--- include/radDetectorConstruction.hh
+++ include/radDetectorConstruction.hh
@@ -5,6 +5,7 @@
 #include "globals.hh"
 
 class G4Box;
+class G4Sphere;
 class G4VPhysicalVolume;
 class G4UniformMagField;
 class G4GenericMessenger;
@@ -17,14 +18,12 @@ public:
   
   virtual G4VPhysicalVolume* Construct();
 
-  void SetNrDetectors( G4int val ){nrDet = val;}
   void SetTargetMaterial(std::string val){targetMaterial = val;}
   void UpdateGeometry();
 
 private:  
   void DefineMaterials();  
   
-  G4int nrDet;
   std::string targetMaterial;
   G4bool  fCheckOverlaps; // option to activate checking of volumes overlaps
 };
